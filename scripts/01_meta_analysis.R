001b822cb31255e8580ebad4fe4e26286e8944a3
Titus Robert Leeyio <titusleeyio@gmail.com>
2026-08-13 17:37:49 +0300

    Set up R project and add initial meta-analysis script

---
+++ scripts/01_meta_analysis.R
@@ -0,0 +1,9 @@
+# Meta-Analysis in R
+# Author: Titus Robert Leeyio
+# Purpose: Reproducible meta-analysis workflow
+
+# Clear the R environment
+rm(list = ls())
+
+# Check R version
+R.version.string
